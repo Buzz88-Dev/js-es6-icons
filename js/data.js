100358458d5feaaba1a3630d6f275454d86b8fd6
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-19 16:50:44 +0200

    aggiornamento

--- js/data.js
+++ js/data.js
@@ -1,4 +1,6 @@
-[
+
+let myArrayObject = [
+
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +114,70 @@
 		color: 'blue'
 	}
 ];
+
+// mi collego al div principale con id container-items
+let myDivContainer = document.getElementById("container_items");
+
+// creo il mio div interno con classe items
+let myDivItems = document.createElement("div");
+myDivItems.classList.add("items");
+
+// creo al suo interno un tag i
+let myi = document.createElement("i");
+
+// e creo al suo interno un tag p
+let myp = document.createElement("p");
+
+console.log(myArrayObject[0].name);
+
+
+for (let i = 0; i < myArrayObject.length; i++){
+
+	let myDivContainer = document.getElementById("container_items");
+
+	let myDivItems = document.createElement("div");
+	myDivItems.classList.add("items");
+
+	let myi = document.createElement("i");
+	myi.classList.add(myArrayObject[i].family)
+	myi.classList.add(myArrayObject[i].prefix + myArrayObject[i].name);
+	
+	if (myArrayObject[i].type === "animal"){
+		myi.classList.add("blue");
+	} else if (myArrayObject[i].type === "vegetable"){
+		myi.classList.add("orange");
+	} else {
+		myi.classList.add("violet");
+	}
+	
+
+	let myp = document.createElement("p");
+	let inseriscoName = myArrayObject[i].name;
+	myp.append(inseriscoName);
+
+
+	myDivItems.append(myi, myp);
+	myDivContainer.append(myDivItems);
+}
+
+
+// myArrayObject.forEach((element) => {
+
+// 	console.log(element.type);
+// 	if (element.type === 'animal'){
+// 		myi.classList.add("blue");
+// 	}
+// })
+
+// let animalBlue = myArrayObject.filter((element) => {
+// 	if(element.type === 'animal') {
+// 		myi.classList.add("blue");
+// 	}
+// })
+
+
+
+
+
+
+
